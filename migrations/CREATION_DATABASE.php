1a266a32d0219d65881eecc61bb298e8bbe9b9d8
elpiou22 <elpiou2@gmail.com>
2025-08-17 22:53:13 +0200

    creation db

---
+++ migrations/CREATION_DATABASE.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class CREATIONDATABASE extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE attack (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(255) NOT NULL, map_position INT NOT NULL, date VARCHAR(255) NOT NULL, clan_id VARCHAR(255) NOT NULL, day INT NOT NULL, result VARCHAR(255) NOT NULL, nb_posts INT NOT NULL, tag VARCHAR(255) NOT NULL, attacker_th INT NOT NULL, defender_th INT NOT NULL, percentage INT NOT NULL, attack_stars INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE clans (id INT AUTO_INCREMENT NOT NULL, clan_id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE param_request (id INT AUTO_INCREMENT NOT NULL, clan_id VARCHAR(255) NOT NULL, date VARCHAR(255) NOT NULL, parameters VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE password_reset_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(100) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', used_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_C5D0A95AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, text VARCHAR(255) NOT NULL, author INT NOT NULL, date DATETIME NOT NULL, upvote INT NOT NULL, downvote INT NOT NULL, vote_weight INT NOT NULL, reply_of INT NOT NULL, nb_replies INT NOT NULL, clan_id VARCHAR(255) NOT NULL, cwl_id VARCHAR(255) NOT NULL, player_map_position INT NOT NULL, day INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, birthdate DATETIME NOT NULL, profile_picture VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE password_reset_request ADD CONSTRAINT FK_C5D0A95AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE password_reset_request DROP FOREIGN KEY FK_C5D0A95AA76ED395');
+        $this->addSql('DROP TABLE attack');
+        $this->addSql('DROP TABLE clans');
+        $this->addSql('DROP TABLE param_request');
+        $this->addSql('DROP TABLE password_reset_request');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE user');
+    }
+}
