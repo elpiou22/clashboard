487fbc2e6f8167e023062a55006472a79205d108
elpiou22 <elpiou2@gmail.com>
2025-08-24 20:08:17 +0200

    changement form Form

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('pseudo')
+            ->add('birthdate', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('profilePictureFile', VichImageType::class, [
+                'label' => 'Photo de profil',
+                'required' => false,
+                'download_uri' => false,
+                'image_uri' => false,
+            ])
+
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+
+            ->add('password', PasswordType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
